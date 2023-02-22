6d110f13e114566c536cd609528f8315d22f533b
Juliana Aigueira <121833579+JulianaAigueira@users.noreply.github.com>
2023-02-22 17:21:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,27 @@ from kivy.app import App
 from kivy.lang import Builder
 from telas import *
 from botoes import *
+import requests
+from myfirebase import MyFirebase
 
 GUI = Builder.load_file('main.kv')
 
 class MainApp(App):
-
+    id_usuario = 1
     def build(self):
+        self.firebase = MyFirebase()
         return GUI
+
+    def on_start(self):
+        requisicao = requests.get(f'https://appcadastrofuncionario-default-rtdb.firebaseio.com/{self.id_usuario}.json')
+        requisicao_dic = requisicao.json()
+        foto = requisicao_dic['foto']
+        #foto_admin = self.root.ids['foto_admin']
+        #foto_admin.source = f'icones/{foto}'
 
     def mudar_tela(self, id_tela):
         gerenciador_telas = self.root.ids['screen_manager']
         gerenciador_telas.current = id_tela
 
 MainApp().run()
-
-
-
 
