a6a235c9db0789d9a928bebb350b3d552aaf04e9
Juliana Aigueira <121833579+JulianaAigueira@users.noreply.github.com>
2023-02-23 20:30:22 -0300

    Update main.py
    nada modificado

--- main.py
+++ main.py
@@ -8,17 +8,22 @@ from myfirebase import MyFirebase
 GUI = Builder.load_file('main.kv')
 
 class MainApp(App):
-    id_usuario = 1
+    id_usuario = 'admin'
     def build(self):
         self.firebase = MyFirebase()
         return GUI
 
     def on_start(self):
+        # pegar informação do adimin
         requisicao = requests.get(f'https://appcadastrofuncionario-default-rtdb.firebaseio.com/{self.id_usuario}.json')
         requisicao_dic = requisicao.json()
+
+        #preencher foto de ferfil
         foto = requisicao_dic['foto']
-        #foto_admin = self.root.ids['foto_admin']
-        #foto_admin.source = f'icones/{foto}'
+        foto_admin = self.root.ids['foto_admin']
+        foto_admin.source = f'icones/{foto}'
+        #print(requisicao_dic)
+
 
     def mudar_tela(self, id_tela):
         gerenciador_telas = self.root.ids['screen_manager']
