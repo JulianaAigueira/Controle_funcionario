67c039302d435e06be59616948776ccac0954e06
Juliana Aigueira <121833579+JulianaAigueira@users.noreply.github.com>
2023-02-23 20:30:07 -0300

    Update myfirebase.py
    arquivo do banco de dados onde estão as funções

--- myfirebase.py
+++ myfirebase.py
@@ -1,4 +1,6 @@
 # site do link API para verificação de e-mail: https://cloud.google.com/identity-platform/docs/use-rest-api?hl=pt-br
+import json
+
 import requests
 from kivy.app import App
 
@@ -29,6 +31,13 @@ class MyFirebase():
 
             with open('refreshtoken.txt', 'w') as arquivo:
                 arquivo.write(refresh_token)
+
+            #link do banco de dados
+            link = f'https://appcadastrofuncionario-default-rtdb.firebaseio.com/{local_id}.json'
+            info_admin = '{"admin": "", "email": "", "foto": "admin.png", "senha": ""}'
+            requisicao_admin = requests.patch(link, data=info_admin)
+            meu_aplicativo.mudar_tela('menupage')
+
         else:
             mensagem_erro = requisicao_dic['error']['message']
             meu_aplicativo = App.get_running_app()
@@ -40,3 +49,35 @@ class MyFirebase():
     def fazer_login(self, email, senha):
 
         pass
+
+    def adicionar_funcinario(self, nome, cpf, endereco, uf, cidade, admissao, cargo, salario, demissao):
+        link = 'https://appcadastrofuncionario-default-rtdb.firebaseio.com/'
+        info = {'nome': nome,
+                'CPF': cpf,
+                'endereço': endereco,
+                'uf': uf,
+                'cidade': cidade,
+                'admissão': admissao,
+                'cargo': cargo,
+                'salário': salario,
+                'demissão': demissao
+                }
+
+        requisicao = requests.post(f'{link}/funcionário/.json', data=json.dumps(info))
+        print(requisicao)
+        print(requisicao.text)
+
+    def buscarfuncionario(self, cpf):
+        link = 'https://appcadastrofuncionario-default-rtdb.firebaseio.com/'
+        requisicao = requests.get(f'{link}/funcionário/.json')
+        requisicao_dic = requisicao.json()
+        #print(requisicao_dic)
+        id_cpf = None
+        for id_funcionario in requisicao_dic:
+            funcionario = requisicao_dic[id_funcionario]['CPF']
+            if funcionario == cpf:
+                print(id_funcionario)
+                id_cpf = id_funcionario
+                resultado = requests.get(f'{link}/funcionário/{id_cpf}/.json')
+
+        self.ids['buscarfuncionariopage'].ids.resultado_busca.text = 5
