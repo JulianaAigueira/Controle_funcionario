ff43e3046cf7c8d656be7a11e36497fd79ff0c40
Juliana Aigueira <121833579+JulianaAigueira@users.noreply.github.com>
2023-02-26 20:35:31 -0300

    Update main.py
    fiz alguns teste, mais não mudei nada

--- main.py
+++ main.py
@@ -8,22 +8,32 @@ from myfirebase import MyFirebase
 GUI = Builder.load_file('main.kv')
 
 class MainApp(App):
-    id_usuario = 'admin/1'
+    #id_usuario = 'admin/1'
     def build(self):
         self.firebase = MyFirebase()
         return GUI
 
     def on_start(self):
-        # pegar informação do adimin
-        requisicao = requests.get(f'https://appcadastrofuncionario-default-rtdb.firebaseio.com/{self.id_usuario}.json')
-        requisicao_dic = requisicao.json()
+        try:
+            with open('refreshtoken.txt', 'r') as arquivo:
+                refresh_token = arquivo.read()
+            local_id, id_token = self.firebase.trocar_token(refresh_token)
+            self.local_id = local_id
+            self.id_token = id_token
 
-        #preencher foto de ferfil
-        #foto = requisicao_dic['foto']
-        #foto_admin = self.root.ids['foto_admin']
-        #foto_admin.source = f'icones/{foto}'
-        #print(requisicao_dic)
+            # pegar informação do adimin
+            requisicao = requests.get(f'https://appcadastrofuncionario-default-rtdb.firebaseio.com/{self.local_id}.json')
+            requisicao_dic = requisicao.json()
 
+            #preencher foto de ferfil
+            foto = requisicao_dic['foto']
+            foto_admin = self.root.ids['foto_admin']
+            foto_admin.source = f'icones/{foto}'
+            print(requisicao_dic)
+
+            self.mudar_tela('menupage')
+        except:
+            pass
 
     def mudar_tela(self, id_tela):
         gerenciador_telas = self.root.ids['screen_manager']
